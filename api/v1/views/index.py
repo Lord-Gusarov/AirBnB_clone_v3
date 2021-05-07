85ffae12d1f0954a0e776869f54652ca88423d0f
fabo893 <jose.rosa893@gmail.com>
2021-05-07 22:22:08 +0000

    Trying to retrieve the number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,18 @@ Index for our web flask
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes_all = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+               "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -13,3 +25,15 @@ def status():
     """
     status = {'status': 'OK'}
     return jsonify(status)
+
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def obj_count():
+    """ 
+    Retrieves the number of each objects
+    """
+    obj_cnt = {}
+    for key in classes_all:
+        obj_cnt[key] = storage.count(key)
+
+    return jsonify(obj_cnt)
