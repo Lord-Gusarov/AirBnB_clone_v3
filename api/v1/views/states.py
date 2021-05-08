5d426febc4e58a7d08fab5fb3c1b085690c7e8e9
Gustavo Hornedo <2367@holbertonschool.com>
2021-05-08 19:53:04 +0000

    Missing name abort is no longer catch by a general exception statement

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,18 +13,19 @@ def list_all_states():
     """
     Returns the list of all State objects
     """
+
     if request.method == 'POST':
+        res_dict = None
         try:
             res_dict = request.get_json()
-            new_state_name = res_dict.get('name')
-            if new_state_name is None:
-                abort(400, description='Missing name')
-            new_state = State(name=new_state_name)
-            new_state.save()
-            return jsonify(new_state.to_dict()), 201
-        except e:
+        except ValueError:
             abort(400, description='Not a JSON')
-
+        new_state_name = res_dict.get('name')
+        if new_state_name is None:
+            abort(400, description='Missing name')
+        new_state = State(name=new_state_name)
+        new_state.save()
+        return jsonify(new_state.to_dict()), 201
     states = storage.all('State')
     list_states_as_dicts = [s.to_dict() for s in states.values()]
     return jsonify(list_states_as_dicts)
